979253d64123ccd96f3c3886b5f5f71749e2ad0a
lucasenders <lend0001@student.monash.edu>
2024-10-04 14:54:17 +1000

    Delete bar_chart.js

--- bar_chart.js
+++
@@ -1,4 +0,0 @@
-var vg_1 = "hw_week_10.vg.json"; 
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-    }).catch(console.error);
